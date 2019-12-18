2d434d743d2089d9ea88a73c8d8cc3821ebe7b02
Rajkumar Rajamanickam <rajkumar.rajamanickam@sifycorp.com>
2019-12-18 17:44:52 +0530

    user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { check,validationResult } = require('express-validator');
 
 router.get('/', (req, res) => res.send('user route'));
 
-router.post('/', (req, res) => {
-    console.log(req);
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter valid email').isEmail()
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    res.send("User post request");
 });
 
 module.exports = router;
